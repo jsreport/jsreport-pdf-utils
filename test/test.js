6cb028d0145541977b3aeeb0bcc45b45f3e9fcb0
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-01-04 18:13:50 +0100

    enable all tests

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ describe('pdf utils', () => {
     return jsreport.init()
   })
 
-  it.only('merge should embed static text', async () => {
+  it('merge should embed static text', async () => {
     await jsreport.documentStore.collection('templates').insert({
       content: '<div style"height: 2cm">header</div>',
       shortid: 'header',
