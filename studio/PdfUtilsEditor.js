f05c16118cd5cd1ce3c05499460a2a230a567a77
BJR Matos <bjrmatos@gmail.com>
2018-12-18 18:55:52 -0500

    use Studio EntityRefSelect control for template selection

--- studio/PdfUtilsEditor.js
+++ studio/PdfUtilsEditor.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Studio from 'jsreport-studio'
 import styles from './style.scss'
 
+const EntityRefSelect = Studio.EntityRefSelect
+
 export default class PdfUtilsEditor extends Component {
   addOperation (entity) {
     Studio.updateEntity(Object.assign({}, entity, { pdfOperations: [...entity.pdfOperations || [], { type: 'merge' }] }))
@@ -32,46 +34,48 @@ export default class PdfUtilsEditor extends Component {
   }
 
   renderOperation (entity, operation, index) {
-    const templates = Studio.getAllEntities()
-      .filter((e) => e.__entitySet === 'templates' && e.shortid !== entity.shortid && e.recipe.includes('pdf'))
-
-    return (<tr key={index}>
-      <td>
-        <select
-          value={operation.templateShortid || 'empty'}
-          onChange={(v) => this.updateOperation(entity, index, { templateShortid: v.target.value })}>
-          <option key='empty' value='empty'>- not selected -</option>
-          {templates.map((e) => <option key={e.shortid} value={e.shortid}>{e.name}</option>)}
-        </select>
-      </td>
-      <td>
-        <select
-          value={operation.type}
-          onChange={(v) => this.updateOperation(entity, index, { type: v.target.value })}>>
-          <option value='merge'>merge</option>
-          <option value='append'>append</option>
-          <option value='prepend'>prepend</option>
-        </select>
-      </td>
-      <td>
-        <input type='checkbox' disabled={operation.type !== 'merge'} checked={operation.mergeToFront === true} onChange={(v) => this.updateOperation(entity, index, { mergeToFront: v.target.checked })} />
-      </td>
-      <td>
-        <input type='checkbox' disabled={operation.type !== 'merge' || operation.mergeWholeDocument} checked={operation.renderForEveryPage === true} onChange={(v) => this.updateOperation(entity, index, { renderForEveryPage: v.target.checked, mergeWholeDocument: false })} />
-      </td>
-      <td>
-        <input type='checkbox' disabled={operation.type !== 'merge' || operation.renderForEveryPage} checked={operation.mergeWholeDocument === true} onChange={(v) => this.updateOperation(entity, index, { mergeWholeDocument: v.target.checked, renderForEveryPage: false })} />
-      </td>
-      <td>
-        <button className='button' style={{ backgroundColor: '#c6c6c6' }} onClick={() => this.removeOperation(entity, index)}><i className='fa fa-times' /></button>
-      </td>
-      <td>
-        {entity.pdfOperations[index - 1] ? <button className='button' style={{ backgroundColor: '#c6c6c6' }} onClick={() => this.moveUp(entity, index)}><i className='fa fa-arrow-up' /></button> : ''}
-      </td>
-      <td>
-        {entity.pdfOperations[index + 1] ? <button className='button' style={{ backgroundColor: '#c6c6c6' }} onClick={() => this.moveDown(entity, index)}><i className='fa fa-arrow-down' /></button> : ''}
-      </td>
-    </tr>)
+    return (
+      <tr key={index}>
+        <td style={{ minWidth: '170px' }}>
+          <EntityRefSelect
+            headingLabel='Select template'
+            filter={(references) => {
+              const templates = references.templates.filter((e) => e.shortid !== entity.shortid && e.recipe.includes('pdf'))
+              return { templates: templates }
+            }}
+            value={operation.templateShortid ? operation.templateShortid : null}
+            onChange={(selected) => this.updateOperation(entity, index, { templateShortid: selected != null && selected.length > 0 ? selected[0].shortid : null })}
+          />
+        </td>
+        <td>
+          <select
+            value={operation.type}
+            onChange={(v) => this.updateOperation(entity, index, { type: v.target.value })}>>
+            <option value='merge'>merge</option>
+            <option value='append'>append</option>
+            <option value='prepend'>prepend</option>
+          </select>
+        </td>
+        <td>
+          <input type='checkbox' disabled={operation.type !== 'merge'} checked={operation.mergeToFront === true} onChange={(v) => this.updateOperation(entity, index, { mergeToFront: v.target.checked })} />
+        </td>
+        <td>
+          <input type='checkbox' disabled={operation.type !== 'merge' || operation.mergeWholeDocument} checked={operation.renderForEveryPage === true} onChange={(v) => this.updateOperation(entity, index, { renderForEveryPage: v.target.checked, mergeWholeDocument: false })} />
+        </td>
+        <td>
+          <input type='checkbox' disabled={operation.type !== 'merge' || operation.renderForEveryPage} checked={operation.mergeWholeDocument === true} onChange={(v) => this.updateOperation(entity, index, { mergeWholeDocument: v.target.checked, renderForEveryPage: false })} />
+        </td>
+        <td>
+          <button className='button' style={{ backgroundColor: '#c6c6c6' }} onClick={() => this.removeOperation(entity, index)}><i className='fa fa-times' /></button>
+        </td>
+        <td>
+          {entity.pdfOperations[index - 1] ? <button className='button' style={{ backgroundColor: '#c6c6c6' }} onClick={() => this.moveUp(entity, index)}><i className='fa fa-arrow-up' /></button> : ''}
+        </td>
+        <td>
+          {entity.pdfOperations[index + 1] ? <button className='button' style={{ backgroundColor: '#c6c6c6' }} onClick={() => this.moveDown(entity, index)}><i className='fa fa-arrow-down' /></button> : ''}
+        </td>
+      </tr>
+    )
   }
 
   renderOperations (entity) {
@@ -97,20 +101,22 @@ export default class PdfUtilsEditor extends Component {
   render () {
     const { entity } = this.props
 
-    return (<div className='block custom-editor' style={{ overflowX: 'auto' }}>
-      <h1><i className='fa fa-file-pdf-o' /> pdf operations
-      </h1>
-      <p style={{ marginTop: '1rem' }}>
-        Use merge/append operations to add dynamic headers or concatenate multiple pdf reports into one.
-        See more docs and examples <a href='https://jsreport.net/learn/pdf-utils'>here</a>.
-      </p>
-      <div style={{ marginTop: '1rem' }}>
-        {this.renderOperations(entity)}
+    return (
+      <div className='block custom-editor' style={{ overflowX: 'auto' }}>
+        <h1><i className='fa fa-file-pdf-o' /> pdf operations
+        </h1>
+        <p style={{ marginTop: '1rem' }}>
+          Use merge/append operations to add dynamic headers or concatenate multiple pdf reports into one.
+          See more docs and examples <a href='https://jsreport.net/learn/pdf-utils'>here</a>.
+        </p>
+        <div style={{ marginTop: '1rem' }}>
+          {this.renderOperations(entity)}
+        </div>
+        <div style={{ marginTop: '1rem' }}>
+          <button className='button confirmation' onClick={() => this.addOperation(entity)}>Add operation</button>
+        </div>
       </div>
-      <div style={{ marginTop: '1rem' }}>
-        <button className='button confirmation' onClick={() => this.addOperation(entity)}>Add operation</button>
-      </div>
-    </div>)
+    )
   }
 }
 
