17d696d19f2d23697592616f843a566ed337e585
BJR Matos <bjrmatos@gmail.com>
2020-03-13 14:33:38 -0500

    integrate pdf sign to the backend

--- test/test.js
+++ test/test.js
@@ -2,16 +2,24 @@ const JsReport = require('jsreport-core')
 const parsePdf = require('../lib/utils/parsePdf')
 const fs = require('fs')
 const path = require('path')
-require('should')
+const { extractSignature } = require('node-signpdf/dist/helpers.js')
+const should = require('should')
 
 function initialize (strategy = 'in-process') {
-  const jsreport = JsReport({ templatingEngines: { strategy } })
+  const jsreport = JsReport({
+    encryption: {
+      secretKey: '1111111811111118'
+    },
+    templatingEngines: { strategy }
+  })
+
   jsreport.use(require('jsreport-templates')())
   jsreport.use(require('jsreport-chrome-pdf')({
     launchOptions: {
       args: ['--no-sandbox']
     }
   }))
+  jsreport.use(require('jsreport-assets')())
   jsreport.use(require('jsreport-phantom-pdf')())
   jsreport.use(require('jsreport-handlebars')())
   jsreport.use(require('jsreport-jsrender')())
@@ -1201,6 +1209,113 @@ describe('pdf utils', () => {
       .and.containEql('Foo-keywords')
       .and.containEql('Foo-creator')
       .and.containEql('Foo-producer')
+  })
+
+  it('pdfSign should sign output pdf', async () => {
+    const result = await jsreport.render({
+      template: {
+        content: 'Hello',
+        engine: 'none',
+        recipe: 'chrome-pdf',
+        pdfSign: {
+          certificateAsset: {
+            content: fs.readFileSync(path.join(__dirname, 'certificate.p12')),
+            password: 'node-signpdf'
+          }
+        }
+      }
+    })
+
+    const { signature, signedData } = extractSignature(result.content)
+    signature.should.be.of.type('string')
+    signedData.should.be.instanceOf(Buffer)
+  })
+
+  it('pdfSign should be able to sign with reference to stored asset', async () => {
+    await jsreport.documentStore.collection('assets').insert({
+      name: 'certificate.p12',
+      shortid: 'certificate',
+      content: fs.readFileSync(path.join(__dirname, 'certificate.p12')),
+      pdfSign: {
+        passwordRaw: 'node-signpdf'
+      }
+    })
+
+    const result = await jsreport.render({
+      template: {
+        content: 'Hello',
+        engine: 'none',
+        recipe: 'chrome-pdf',
+        pdfSign: {
+          certificateAssetShortid: 'certificate'
+        }
+      }
+    })
+
+    const { signature, signedData } = extractSignature(result.content)
+    signature.should.be.of.type('string')
+    signedData.should.be.instanceOf(Buffer)
+  })
+
+  it('pdfSign should crypt asset password before insert', async () => {
+    const res = await jsreport.documentStore.collection('assets').insert({
+      name: 'a',
+      content: 'hello',
+      engine: 'none',
+      recipe: 'html',
+      pdfSign: {
+        passwordRaw: 'foo'
+      }
+    })
+
+    should(res.pdfSign.passwordRaw).be.null()
+    res.pdfSign.passwordSecure.should.not.be.eql('foo')
+    res.pdfSign.passwordFilled.should.be.true()
+  })
+
+  it('pdfSign should crypt asset password before update', async () => {
+    await jsreport.documentStore.collection('assets').insert({
+      name: 'a',
+      engine: 'none',
+      recipe: 'html'
+    })
+
+    await jsreport.documentStore.collection('assets').update({ name: 'a' }, {
+      $set: {
+        pdfSign: {
+          passwordRaw: 'foo'
+        }
+      }
+    })
+
+    const entity = await jsreport.documentStore.collection('assets').findOne({
+      name: 'a'
+    })
+
+    should(entity.pdfSign.passwordRaw).be.null()
+    entity.pdfSign.passwordSecure.should.not.be.eql('foo')
+    entity.pdfSign.passwordFilled.should.be.true()
+  })
+
+  it('pdfSign should use asset encoding when inline in the request', async () => {
+    const result = await jsreport.render({
+      template: {
+        content: 'Hello',
+        engine: 'none',
+        recipe: 'chrome-pdf',
+        pdfSign: {
+          certificateAsset: {
+            content: fs.readFileSync(path.join(__dirname, 'certificate.p12')).toString('base64'),
+            encoding: 'base64',
+            password: 'node-signpdf'
+          }
+        }
+      }
+    })
+
+    const { signature, signedData } = extractSignature(result.content)
+    signature.should.be.of.type('string')
+    signedData.should.be.instanceOf(Buffer)
   })
 })
 
